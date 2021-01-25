5bb1d0c98daa5a13eccd4cb5b2e2bf02a3e24377
Andrea Mock <57298825+amock412@users.noreply.github.com>
2021-01-25 21:03:19 +0100

    Completed module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import routes from './src/routes/routes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
@@ -17,13 +17,8 @@ mongoose.connect('mongodb://localhost/productsdb', {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 
-// routes 
 routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
-);
-
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
 );
