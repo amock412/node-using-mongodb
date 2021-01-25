79c4a912d359b3ee0aac1d8c3183bd083f14ec80
Andrea Mock <57298825+amock412@users.noreply.github.com>
2021-01-25 10:17:48 +0100

    Completed module

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 4000;
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/productsdb';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
